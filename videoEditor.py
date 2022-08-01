f7a21ce91bc8d454cc343120ff48e758124f9926
NickRTR <95316598+NickRTR@users.noreply.github.com>
2022-08-01 23:08:08 +0200

    Add background music

--- videoEditor.py
+++ videoEditor.py
@@ -1,10 +1,12 @@
 import moviepy.editor as movie
 import moviepy.video as video
+import moviepy.audio as audio
 import glob
 import random
 
 audioComments = glob.glob("./audio/comment-*.mp3")
-backgroundVideos = glob.glob("./background/*.mp4")
+backgroundVideos = glob.glob("./background/video/*.mp4")
+backgroundMusic = glob.glob("./background/music/*.mp3")
 
 questionAudio = f"./audio/question.mp3"
 outputVideo = f"./output/output.mp4"
@@ -23,6 +25,13 @@ def joinAudioWithVideo(audioClip):
     # change resolution to 1080x1920
     return videoClip.set_audio(audioClip)
 
+def addBackgroundMusic(videoClip):
+    music = movie.AudioFileClip(random.choice(backgroundMusic))
+    music = audio.fx.all.volumex(music, 0.5)
+    joinedAudio = movie.CompositeAudioClip([videoClip.audio, music])
+    videoClip.audio = joinedAudio
+    return videoClip
+
 def editVideo(finalVideo, audioDuration):
     editedVideo = finalVideo.resize((1080, 1920))
     # loop background video to fit audio length
@@ -32,10 +41,20 @@ def editVideo(finalVideo, audioDuration):
     # fade out
     editedVideo = video.fx.all.fadeout(editedVideo, .1)
 
+    # # add text
+    # textClip = movie.TextClip("Test", fontsize = 75, color = 'black') 
+        
+    # # setting position of text in the center and duration will be 10 seconds 
+    # textClip= textClip.set_pos('center').set_duration(10) 
+        
+    # # Overlay the text clip on the first video clip 
+    # editedVideo = movie.CompositeVideoClip([editedVideo, textClip]) 
+
     return editedVideo
 
 def createVideo():
     audio = joinQuestionWithComments()
     finalVideo = joinAudioWithVideo(audio)
+    finalVideo = addBackgroundMusic(finalVideo)
     finalVideo = editVideo(finalVideo, audio.duration)
     finalVideo.write_videofile(outputVideo, fps=30)
