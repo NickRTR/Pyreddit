73d3996f8578b91438b17ec8d46db35cf1c4751d
NickRTR <95316598+NickRTR@users.noreply.github.com>
2023-01-23 17:01:55 +0100

    Update project

--- scripts/main.py
+++
@@ -1,9 +0,0 @@
-from helper import deleteTemporaryFiles, deleteFiles
-from redditScraper import getContent
-
-print("delete previous output videos")
-deleteFiles("../output")
-
-getContent(1)
-
-deleteTemporaryFiles()
